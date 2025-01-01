6f1fc19050b71662d399bbfafa00e8dbcc277c3b
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2025-01-01 12:59:18 +0300

    Update navigate.js
    3 попытка убрать пароль

--- scripts/navigate.js
+++ scripts/navigate.js
@@ -1,12 +1,14 @@
 <!-- Скрипт navigate.js -->
 let isOnDebugPage = window.location.pathname.endsWith("debug.html");
 
-const navigateToDebug = (refreshOnly = false) => {
+const navigateToDebug = () => {
     if (isOnDebugPage) {
+        // Если мы уже на странице отладки, просто обновляем её
         location.reload();
         return;
     }
 
+    // Если мы не на странице отладки, запрашиваем пароль
     const password = prompt("Введите пароль для доступа к странице отладки:");
     if (password === "0000") {
         window.location.href = "debug.html";
